330c0d783c72cfde1b9376e4ddf7b780c48a6fa4
Ilya Postnikov <sewerefd@gmail.com>
2024-12-11 15:55:48 +0700

    feat: add migrations of DB

---
+++ migrations/0001_create_tables.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users(
+    uuid UUID PRIMARY KEY,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions(
+  id SERIAL PRIMARY KEY,
+  user_id UUID references users (uuid),
+  refresh_token VARCHAR(40),
+  expired_at_refresh TIMESTAMP without time zone,
+  ip VARCHAR(15) NOT NULL
+);
